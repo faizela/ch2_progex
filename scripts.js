60533abec6ba71394b78bdbf22c24d3a585fd4a0
Feisal Ahmed <feisala132@gmail.com>
2024-02-18 03:27:32 +0000

    Completed exercise two counting the number of characters

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
 // Exercise 1 - Saying hello
-
+/*
 let username = prompt("What is your name? ")
 
 let msg = 'Hello, ' + username + ', ' + 'nice to meet you!'
 
 console.log(msg)
+*/
+
+// challenge 1 - saying hello
+
+// challenge 2 - saying hello
 
 
-// challenge 1
+// Exercise 2 - Counting the number of characters
+
+/*
+let str = prompt('What is the input string')
+console.log(str + ' has ' + str.length + ' characters.')
+*/
+
+// Challenge 1 - Counting the number of characters
+
+// Challenge 2 - Counting the number of characters
